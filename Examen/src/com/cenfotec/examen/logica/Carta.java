133cb157fac501d82a2fb29b8c805c694f8e5325
shannon1720 <39140859+shannon1720@users.noreply.github.com>
2018-06-13 21:51:47 -0600

    Add files via upload
    refactoring

--- Examen/src/com/cenfotec/examen/logica/Carta.java
+++ Examen/src/com/cenfotec/examen/logica/Carta.java
@@ -1,36 +1,54 @@
 package com.cenfotec.examen.logica;
 
 public class Carta {
-private String nombre;
-private String palo;
-private int valor;
+	private String nombre;
+	private String palo;
+	private int valor;
 
+	public int crearCarta(int valor, String nombre, String palo) throws Exception {
+		int numero = 0;
 
-public int crearCarta(int valor,String nombre,String palo) throws Exception {
-	int numero=0;
-	
-	if(valor <1 || valor>10 ) {	
-	
-		throw new Exception("No es un valor valido.");
-		
+		validarValorCarta(valor);
+
+		numero = validarnombre(nombre, numero);
+
+		numero = validarPalo(palo, numero);
+
+		return numero;
 	}
-	
-	if(nombre=="AS"||nombre=="Dos"||nombre=="Tres"||nombre=="Cuatro"||nombre=="Cinco"||nombre=="Seis"||nombre=="Siete"||nombre=="Ocho"||nombre=="Nueve"||nombre=="Jota"||nombre=="Quina"||nombre=="Ka") {
+
+	private int validarPalo(String palo, int numero) throws Exception {
 		
-		numero=1;
+		String nombresPalo[]= {"Escudos","Flores","Estrellas","Gotas"};
+		
+		for(int i=0;i<nombresPalo.length;i++) {
+		if (nombresPalo[i]==palo) {
+
+			numero = 1;
+
+		} 
+		}
+		return numero;
 	}
-	
-if(palo=="Escudos"|| palo=="Flores"||palo=="Estrellas"||palo=="Gotas") {
-	
-	numero=1;
-	
-}else {
-	
-	throw new Exception("No es un valor valido.");
-}
 
+	private int validarnombre(String nombre, int numero) {
+		
+		String nombreCarta[]= {"AS","Dos","Tres","Cuatro","Cinco","Seis","Siete","Ocho","Nueve","Jota","Quina","Ka"};
+		for(int i=0;i<nombreCarta.length;i++) {
+		if (nombreCarta[i]==nombre ) {
 
-	return numero;
-}
+			numero = 1;
+		}}
+		return numero;
+	}
+		
+
+	private void validarValorCarta(int valor) throws Exception {
+		if (valor < 1 || valor > 10) {
+
+			throw new Exception("No es un valor valido.");
+
+		}
+	}
 
 }
