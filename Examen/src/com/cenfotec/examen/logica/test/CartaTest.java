1cdb7f937177bf11ba44a8efa931c60d23724199
shannon1720 <39140859+shannon1720@users.noreply.github.com>
2018-06-14 11:29:45 -0600

    Add files via upload
    Paso2

--- Examen/src/com/cenfotec/examen/logica/test/CartaTest.java
+++ Examen/src/com/cenfotec/examen/logica/test/CartaTest.java
@@ -22,14 +22,30 @@ public void nombreCarta() throws Exception {
 		assertEquals(1,micarta.crearCarta(11,"As","Escudos"));
 	}
 
+	
+	
+	
 	@Test
   public void esIgual() throws Exception {
 	  
-	Carta	micarta1=new Carta(3,"Dos","Flores");
+	    Carta micarta1=new Carta(2,"Dos","Flores");
 		Carta micarta2=new Carta(2,"Dos","Flores");
 		
-		boolean esdiferente=micarta.equals(micarta2);
+		boolean esdiferente=micarta1.equals(micarta2);
 		
-	  assertEquals(false,esdiferente);
+	  assertEquals(true,esdiferente);
 	  
-  }}
+  }
+	
+	@Test
+	  public void esDiferente() throws Exception {
+		  
+		    Carta micarta1=new Carta(3,"Dos","Flores");
+			Carta micarta2=new Carta(2,"Dos","Flores");
+			
+			boolean esigual=micarta1.equals(micarta2);
+			
+		  assertEquals(false,esigual);
+		  
+	  }
+}
